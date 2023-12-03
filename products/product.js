13e6a29f894fc075855321b64cca8bd6b0b28fb3
deshapriya <deshapriyakhatua@gmail.com>
2023-12-03 12:06:14 +0530

    changes done

--- products/product.js
+++ products/product.js
@@ -3,5 +3,4 @@ document.querySelector(".filter__options_container").addEventListener("click",()
     document.querySelector(".main__left").classList.toggle("hide_filter_container");
     document.querySelector(".main__left").classList.toggle("show_filter_container");
     document.querySelector(".filter_dropdown_arrow").classList.toggle("rotate");
-    console.log(document.querySelector(".main_left"))
 });
